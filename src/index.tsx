1576854e7dfb012e978aebafd853a2ae89202f11
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-01-29 23:14:37 +0000

    remove reload checkbox from HTML

--- src/index.tsx
+++ src/index.tsx
@@ -53,16 +53,6 @@ htmlServer.get("*", (c) => {
   return c.html(
     <html>
       <body>
-        <label htmlFor="reload">
-          <input
-            type="checkbox"
-            id="reload"
-            defaultChecked={reload}
-            style={{ marginRight: 4 }}
-          />
-          Reload automatically
-        </label>
-
         <div id="plot"></div>
         <script src="https://cdn.jsdelivr.net/npm/d3@7"></script>
         <script src="https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6"></script>
